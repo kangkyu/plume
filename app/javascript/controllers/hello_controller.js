de822582fcbcf6964d7f19dfeca8b27bbe270960
kangkyu <kangkyu1111@gmail.com>
2022-04-23 23:52:06 -0700

    Add launch index action to show some json

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -3,5 +3,13 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   connect() {
     this.element.textContent = "Hello World!"
+
+    this.load()
+  }
+
+  load() {
+    fetch("https://api.github.com/users/kangkyu/gists?per_page=100")
+      .then(response => response.text())
+      .then(body => this.element.innerHTML = body)
   }
 }
